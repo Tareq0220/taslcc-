b4ff767c15dcddb128eb2da1d4f99fcad87e0d80
Tareq0220 <106815737+Tareq0220@users.noreply.github.com>
2024-12-30 21:54:51 +0300

    Add files via upload

---
+++ FunDevC#/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FunDevC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(7); 
+            Shape rectangle = new Rectangle(5, 2); 
+
+           Console.WriteLine("Area of Circle: " + circle.CalculateArea());
+           Console.WriteLine("Area of Rectangle: " + rectangle.CalculateArea());
+
+        }
+    }
+}
---
+++ FunDevC#/ShapeInterfaceExercise.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunDevC_
+{
+    
+    public interface Shape
+    {
+        double CalculateArea();
+    }
+
+   
+    public class Circle : Shape
+    {
+        public double Radius { get; private set; } 
+
+       
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius; 
+        }
+    }
+
+    
+    public class Rectangle : Shape
+    {
+        public double Length { get;set; } 
+        public double Width { get;set; }  
+
+       
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+       
+        public double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    
+    
+}
